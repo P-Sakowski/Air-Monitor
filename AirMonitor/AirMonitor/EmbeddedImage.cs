655a92f1b9e6515e42eab5083c9be5fb5a965403
Przemysław Sakowski <p.sakowski@protonmail.com>
2020-05-26 18:44:05 +0200

    Ex1 Finished: Add basic XAML Interface, add embedded resource

---
+++ AirMonitor/AirMonitor/EmbeddedImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AirMonitor
+{
+    class EmbeddedImage : IMarkupExtension
+    {
+        public string ResourceId { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (String.IsNullOrWhiteSpace(ResourceId))
+            {
+                return null;
+            }
+            return ImageSource.FromResource(ResourceId);
+        }
+    }
+}
---
+++ AirMonitor/AirMonitor/Views/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AirMonitor
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Help_Click(object sender, EventArgs e)
+        {
+            DisplayAlert("Co to jest CAQI?", "Indeks jakości powietrza jest wykorzystywany przez agencje rządowe do informowania opinii publicznej o tym, jak zanieczyszczone jest obecnie powietrze lub jak przewiduje się jego zanieczyszczenie. Zagrożenia dla zdrowia publicznego rosną wraz ze wzrostem CAQI.", "Zamknij");
+        }
+    }
+}
