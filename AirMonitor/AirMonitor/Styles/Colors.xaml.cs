ef894087ec40afb4bac188368c017732fe314d4c
Przemysław Sakowski <p.sakowski@protonmail.com>
2020-05-27 00:05:28 +0200

    Added styles and colors definition, styles and colors moved to separate XAML files

---
+++ AirMonitor/AirMonitor/Styles/Colors.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AirMonitor
+{
+    public partial class Colors : ResourceDictionary
+    {
+        public Colors()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AirMonitor/AirMonitor/Styles/Styles.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AirMonitor
+{
+    public partial class Styles : ResourceDictionary
+    {
+        public Styles()
+        {
+            InitializeComponent();
+        }
+    }
+}
